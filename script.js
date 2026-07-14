9288e53aacae46ffb60ecde4dabc97ce54b89598
smarttools <kidsnurseryrhymes0@gmail.com>
2026-07-14 18:38:52 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("AI Tools Hub Loaded");
+const input = document.querySelector('input[type="file"]');
+const preview = document.getElementById('preview');
+
+if(input && preview){
+    input.addEventListener("change",function(){
+
+        const file=this.files[0];
+
+        if(file){
+
+            preview.src=URL.createObjectURL(file);
+
+            preview.style.display="block";
+
+        }
+
+    });
+
+}
