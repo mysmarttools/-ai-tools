51ea6641eb1d6750b23fe21b10cff3c3bd647548
smarttools <kidsnurseryrhymes0@gmail.com>
2026-07-14 18:56:57 +0500

    Update script.js

--- script.js
+++ script.js
@@ -11,3 +11,18 @@ if (fileInput && preview) {
         }
     });
 }
+
+function generateMeta() {
+
+    const keyword = document.getElementById("keyword").value;
+    const brand = document.getElementById("brand").value;
+
+    if(keyword==="" || brand===""){
+        alert("Please enter Keyword and Website Name");
+        return;
+    }
+
+    document.getElementById("result").value =
+    `Looking for ${keyword}? Shop premium products at ${brand}. Discover high-quality products, fast delivery, and great prices today.`;
+
+}
