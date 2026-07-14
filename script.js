b897192bbffe89b2631c95abfccb9ce9a06fb089
smarttools <kidsnurseryrhymes0@gmail.com>
2026-07-14 18:46:43 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,13 @@
-const input = document.querySelector('input[type="file"]');
+const fileInput = document.querySelector('input[type="file"]');
 const preview = document.getElementById('preview');
 
-if(input && preview){
-    input.addEventListener("change",function(){
+if (fileInput && preview) {
+    fileInput.addEventListener('change', function () {
+        const file = this.files[0];
 
-        const file=this.files[0];
-
-        if(file){
-
-            preview.src=URL.createObjectURL(file);
-
-            preview.style.display="block";
-
+        if (file) {
+            preview.src = URL.createObjectURL(file);
+            preview.style.display = 'block';
         }
-
     });
-
 }
