5777a77d7bc51926f769ad37709f1c5666c2c16e
aitoolshub <kidsnurseryrhymes0@gmail.com>
2026-07-16 14:52:27 +0500

    Update script.js

--- script.js
+++ script.js
@@ -491,3 +491,115 @@ link.href=canvas.toDataURL();
 link.click();
 
 }
+
+// Meta Title
+const metaTitle = document.getElementById("metaTitle");
+
+// Meta Description
+const metaDescription = document.getElementById("metaDescription");
+
+// Counters
+const titleCount = document.getElementById("titleCount");
+const descCount = document.getElementById("descCount");
+
+// Status
+const titleStatus = document.getElementById("titleStatus");
+const descStatus = document.getElementById("descStatus");
+
+function checkLength(){
+
+    // ========= META TITLE =========
+
+    const titleLength = metaTitle.value.length;
+
+    titleCount.innerHTML = titleLength;
+
+    if(titleLength == 0){
+
+        titleStatus.innerHTML = "⚪ Enter Meta Title";
+
+        titleStatus.style.color = "gray";
+
+    }
+
+    else if(titleLength < 30){
+
+        titleStatus.innerHTML = "❌ Too Short";
+
+        titleStatus.style.color = "red";
+
+    }
+
+    else if(titleLength <= 49){
+
+        titleStatus.innerHTML = "⚠ Good (Can Improve)";
+
+        titleStatus.style.color = "orange";
+
+    }
+
+    else if(titleLength <= 60){
+
+        titleStatus.innerHTML = "✅ Perfect";
+
+        titleStatus.style.color = "green";
+
+    }
+
+    else{
+
+        titleStatus.innerHTML = "❌ Too Long";
+
+        titleStatus.style.color = "red";
+
+    }
+
+
+
+    // ========= META DESCRIPTION =========
+
+    const descLength = metaDescription.value.length;
+
+    descCount.innerHTML = descLength;
+
+    if(descLength == 0){
+
+        descStatus.innerHTML = "⚪ Enter Meta Description";
+
+        descStatus.style.color = "gray";
+
+    }
+
+    else if(descLength < 120){
+
+        descStatus.innerHTML = "❌ Too Short";
+
+        descStatus.style.color = "red";
+
+    }
+
+    else if(descLength <= 139){
+
+        descStatus.innerHTML = "⚠ Good (Can Improve)";
+
+        descStatus.style.color = "orange";
+
+    }
+
+    else if(descLength <= 160){
+
+        descStatus.innerHTML = "✅ Perfect";
+
+        descStatus.style.color = "green";
+
+    }
+
+    else{
+
+        descStatus.innerHTML = "❌ Too Long";
+
+        descStatus.style.color = "red";
+
+    }
+
+}
